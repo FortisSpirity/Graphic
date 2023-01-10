eadfff7758dd3ae05bd239b20387da470539c7b9
Mykola Dudchak <nik35092@gmail.com>
2023-01-10 18:15:09 +0200

    Graphic

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    $('.content__btn').on('click', () => {
+      $('.content__btn').toggleClass('active');
+      $('.content__filter').toggleClass('active');
+    });
+
+    $('.aside__burger').on('click', () => {
+      $('.aside').toggleClass('active');
+    });
+
+    $('.filter__change-btn').on('click', function() {
+        $('.filter__change-btn').removeClass('active');         
+        $(this).toggleClass('active');
+    });
+
+    $('.menu__item').on('click', function() {
+        $('.menu__item').removeClass('active');         
+        $(this).toggleClass('active');
+    });
+
+
+
+
+
+      $('input[type="checkbox"]').styler();
+    
+
+
+  });
